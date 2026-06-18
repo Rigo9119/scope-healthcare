9808ee441f3123f64c704f64a27a2b8971e021fc
Rigo Rosero Castillo <rigo4791@gmail.com>
2026-06-18 18:24:57 -0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    paraglideVitePlugin({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+    devtools(),
+    tailwindcss(),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    viteReact(),
+  ],
+});
+
+export default config;
